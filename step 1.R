3aa07984b02d939826302d0c9608ecb15345582b
Zhou <zhoux104@sc1nc003is01.cm.cluster>
2022-10-06 10:01:17 -0700

    update

--- step 1.R
+++ step 1.R
@@ -7,6 +7,11 @@ library(ggplot2)
 library(tools)
 library(readr)
 
+library(foreach)
+library(doParallel)
+registerDoParallel(detectCores())
+ 
+
 process<-function(state){
   #state="tx"
   ######### read in congressional district map 2010
@@ -26,34 +31,27 @@ process<-function(state){
   blockcentroid=st_centroid(block$geometry)
   # district belonging matrix, output a sparse matrix
   belong_mat=matrix(NA,nrow=dim(block)[1],ncol=dim(cong)[1])
-  for(d in 1:dim(cong)[1]){
-    belong_mat[,d]=st_within(blockcentroid, cong[d,], sparse = FALSE)
+  
+  belong_mat = foreach(d = 1:dim(cong)[1], .combine = 'cbind',.packages = "sf")%dopar%{
+    st_within(blockcentroid, cong[d,], sparse = FALSE)
   }
+  
+   
   # add a new colum to the block dataset indicating dist
-  dist=rep(NA,dim(block)[1])
-  
-  library(foreach)
-  foreach (i=1:3) %do% {
-    sqrt(i)
-  }
-  
-  
-  for(i in 1:dim(block)[1]){
-    if(sum(belong_mat[i,]==TRUE)>0){dist[i]=which(belong_mat[i,]==TRUE)}
-    else{
-      dist[i]=NA
-    }
+  dist = foreach(i = 1:dim(block)[1], .combine =c)%dopar%{
+    ifelse(sum(belong_mat[i,]==TRUE)>0,which(belong_mat[i,]==TRUE), NA )
   }
   block$dist=dist
-  
+ 
   ####### read in voters at block level
   file=paste("voter2021/",toupper(state),"_L2_Comm_2010BlockAgg/",toupper(state),"_l2_comm_block_agg_20210419.csv",sep="")
   voter=read_csv(file,col_types = cols(geoid = col_character())) 
   #names(voter)
+   
   
   # link block with voter using geoid, voter live only in half of the region
   block = block%>%left_join(voter,by=c("GEOID"="geoid"))
-  
+  #View(block)
   # remove nonresident blocks
   block = block%>%filter(!is.na(total_reg))
   
@@ -74,27 +72,40 @@ process<-function(state){
     mutate(aa.ratio=(eth1_aa)/total_reg, hisp.ratio=(eth1_hisp)/total_reg )%>%
     mutate(race.group=ifelse(aa.ratio>=0.5,"AA",ifelse(hisp.ratio>=0.5,"Hispanic","Others")))
   
-  
-  
-  return(list(blockpower,p1,p2))
+  return(blockpower)
 }
 tx.data=process("tx")
 tx.data[[2]]
 tx.data[[3]]
 
-p1=st_drop_geometry(blockpower)%>%
+st_drop_geometry(blockpower)%>%
   select(race.group,aa.ratio,hisp.ratio,block.power)%>%
   ggplot()+
-  geom_density(aes(block.power,group=race.group,col=race.group))+
+  geom_density(aes(x=block.power,y=..scaled..,group=race.group,col=race.group))+
   xlim(c(0,0.00001))
 
-
+temp=st_drop_geometry(blockpower)%>%
+  select(eth1_eur,eth1_aa,eth1_hisp,rep.wt,dem.wt, block.power,race.group)%>%
+  filter(race.group %in% c("Hispanic","Others"))%>%
+  filter(!is.na(block.power))%>%
+  mutate(T=ifelse(race.group=="Hispanic",1,0))
+  
+View(temp)
 #### visualize power
 p2=tm_shape(blockpower) +
   tm_polygons("race.group")+
   tm_dots("block.power",size=0.01,alpha=0.1)+
   tm_shape(cong)+
   tm_borders(col='red')
+
+library(qte)
+
+jt.rand <- ci.qtet(block.power ~ T, data=temp, probs=seq(0.05,0.95,0.05), 
+                   se=T, iters=10)
+summary(jt.rand)
+ggqte(jt.rand)
+
+
 
 
  
